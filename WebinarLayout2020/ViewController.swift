b1a3fd997c911d4e66d215847a9581f2b5d41ebd
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-01-12 20:15:25 +0300

    Initial Commit

---
+++ WebinarLayout2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebinarLayout2020
+//
+//  Created by Алексей Пархоменко on 12.01.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
